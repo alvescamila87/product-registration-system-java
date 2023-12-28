43ed838ca07cb516096d5c6f7245b1c57e8b7333
Camila <116912821+alvescamila87@users.noreply.github.com>
2023-12-28 19:29:20 -0300

    c02: create menu method

--- src/com/senai/ProductArray.java
+++ src/com/senai/ProductArray.java
@@ -22,14 +22,29 @@ public class ProductArray {
         
     Scanner entrada = new Scanner (System.in);
     
-    login(entrada);
+    // login do usuário
+    login(entrada);  
+    
+    // menu para opção do usuário
+    int opcao  = 1;
+    
+    while(opcao != 9) {
+         opcao = menu(entrada);
+         
+         switch (opcao) {
+             case 9:
+                 System.out.println("FIM PROGRAMA");
+                 break;
+                         
+         }
+    }          
+    
     
         
     }
     
     // Login 
-    public static void login(Scanner entrada) {
-        
+    public static boolean login(Scanner entrada) {        
     
     String userDataBase = "admin";
     String passwordDataBase = "12345";
@@ -50,7 +65,7 @@ public class ProductArray {
             password = entrada.nextLine();
 
             if (userDataBase.equals(user) && passwordDataBase.equals(password)) {
-                loginValido = true;
+                loginValido = true;                
             } else {
                 throw new Exception();
             }
@@ -61,6 +76,52 @@ public class ProductArray {
         
     }
     
+    return loginValido;
+    
+    }
+    
+    public static int menu(Scanner entrada) {
+        
+        boolean opcaoValida = false;
+        int opcao = 0;
+        String entradaMenu;
+        
+        while(!opcaoValida) {
+            
+            System.out.println("");
+            System.out.println("------------------ MENU ------------------");
+            System.out.println("");
+            System.out.println("""
+                               [1] Insert product
+                               [2] Update product 
+                               [3] Remove product 
+                               [4] Display all products 
+                               [5] Display all products by sorting
+                               [6] Display all products by classification
+                               [7] Add inventory 
+                               [8] Remove inventory 
+                               [9] Exit 
+                               """);
+            System.out.println("------------------------------------------");
+            System.out.println("");
+            
+            try{
+                System.out.println("Digite uma opção do menu: ");
+                entradaMenu = entrada.nextLine();
+                opcao = Integer.parseInt(entradaMenu);
+                
+                if(opcao >= 0 && opcao <= 8) {
+                    opcaoValida = true;
+                } else {
+                    throw new Exception();                    
+                }
+            } catch (Exception e) {
+                System.out.println("Entrada inválida, digite novamente... ");
+            }
+        }
+        
+        return opcao;
+        
     }
     
 }
